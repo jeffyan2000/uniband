ffeac3afcd3075d9e121a64022c0b7508350d1bf
Xuezhi Yan <2000chrisyan@gmail.com>
2020-05-13 18:06:48 -0300

    can now pluck string

--- client/js/band.js
+++ client/js/band.js
@@ -2,12 +2,33 @@ var ctelement = document.getElementById("ctx");
 var ctx = document.getElementById("ctx").getContext("2d");
 var ctxbg = document.getElementById("ctx-bg").getContext("2d");
 ctx.font = '30px Arial';
-var socket = io();
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
 
 var guitar = new InsGuitar();
 function updateCanvas(){
-    ctxbg.clearRect(0,0,1024,768);
+    ctx.clearRect(0,0,1024,768);
     guitar.draw();
+}
+
+
+document.onkeydown = function(event){
+	if (event.keyCode === 80) {
+		guitar.pluck();
+    }
 }
 
 setInterval(updateCanvas,50);
--- client/js/guitar.js
+++ client/js/guitar.js
@@ -1,19 +1,75 @@
-function InsGuitar(id){
-    this.guitarString = new GuitarString();
+class InsGuitar{
+    constructor(id){
+        this.guitarString = new GuitarString(112);
+    }
 
-    this.draw = function(){
+    draw(){
+        this.guitarString.update();
+        
         this.guitarString.draw();
+    }
+
+    pluck(){
+        this.guitarString.pluck(10, 6);
     }
 }
 
-function GuitarString(){
-    this.segments = [];
-    this.pos = [0, 100];
+class GuitarString{
+    constructor(numberOfSegments){
+        this.segments = [];
+        this.seg_len = 5;
+        this.seg_num = numberOfSegments;
+        this.pos = [0, 100];
+        this.initSegments(numberOfSegments);
+        this.length = numberOfSegments * this.seg_len;
+        
+        this.waveHeight = 0;
+        this.maxHeight = 0;
+        this.direction = 0;
+    }
 
-    this.draw = function(){
+    initSegments(number){
+        for(var i = 0; i < number; i++){
+            this.segments.push(0);
+        }
+    }
+
+    draw(){
         ctx.beginPath();
         ctx.moveTo(this.pos[0], this.pos[1]);
-        ctx.lineTo(300, 150);
+        for(var i = 0; i < this.seg_num; i += 1){
+            ctx.lineTo((i+1)*this.seg_len, this.pos[1] + this.segments[i]);
+        }
         ctx.stroke();
+        ctx.closePath();
+    }
+
+    genCurve(){
+        for(var i = 0; i < this.seg_num; i += 1){
+            this.segments[i] = Math.sin(i * this.seg_len/60) * this.waveHeight;
+        }
+    }
+
+    update(){
+        if(this.maxHeight > 0){
+            this.waveHeight += this.direction;
+            if(this.waveHeight < -this.maxHeight){
+                this.maxHeight -= 1;
+                this.direction = -this.direction;
+                this.waveHeight = -this.maxHeight;
+            }
+            else if(this.waveHeight > this.maxHeight){
+                this.maxHeight -= 1;
+                this.direction = -this.direction;
+                this.waveHeight = this.maxHeight;
+            }
+            this.genCurve();
+        }
+    }
+
+    pluck(strength, tensity){
+        this.waveHeight = strength;
+        this.maxHeight = strength;
+        this.direction = -tensity;
     }
 }
